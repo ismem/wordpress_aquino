9882ce641f8360ccbd2a39ced6fa2715bc39bf9c
pedroalatrista <piteryadel.16@gmail.com>
2015-11-23 19:15:34 -0500

    cambiando header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html id="home-version-1">
         <head>
-                <title>WarTa - News/Magazine Template</title>
+                <title>WarTa - News/Magazine Template002</title>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 
