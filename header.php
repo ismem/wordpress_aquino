91078a9c2e1db7780ce945a5d7f266eb7b0aff94
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2015-11-23 19:11:56 -0500

    modifique el header.php

--- header.php
+++ header.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <html id="home-version-1">
         <head>
-                <title>WarTa - News/Magazine Template</title>
+                <title>ISMEM</title>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                
+
                 <!--Import Google font-->
                 <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700' rel='stylesheet' type='text/css'>
-                
-                <!-- Stylesheets -->        
+
+                <!-- Stylesheets -->
                 <link href="<?php bloginfo("template_url") ?>/css/style.min.css" rel="stylesheet">
                 <link href="<?php bloginfo("template_url") ?>/css/custom.css" rel="stylesheet">
 
                 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries
                      ie8.css and ie8.js custom style  and script that needed for IE8. -->
                 <!--[if lt IE 9]>
-                        <link href="css/ie8.css" rel="stylesheet">        
+                        <link href="css/ie8.css" rel="stylesheet">
                         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
                         <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
                         <script src="<?php bloginfo("template_url") ?>/js/ie8.js"></script>
                 <![endif]-->
-        </head>    
-            
+        </head>
+
         <body>
                 <!--HEADER
                 ==========-->
@@ -91,7 +91,7 @@
                                                                 <ul class="dropdown-menu">
                                                                         <li><a href="home-version-1.html">Home Vervion 1</a></li>
                                                                         <li><a href="home-version-2.html">Home Version 2</a></li>
-                                                                        <li class="divider"></li>                                    
+                                                                        <li class="divider"></li>
                                                                         <li><a href="home-version-1-rtl.html">Home Version 1 RTL</a></li>
                                                                         <li><a href="home-version-2-rtl.html">Home Version 2 RTL</a></li>
                                                                 </ul>
@@ -125,7 +125,7 @@
                                                                         <li><a href="review-star-bottom-rtl.html">Review Star Bottom RTL</a></li>
                                                                         <li><a href="gallery-rtl.html">Gallery RTL</a></li>
                                                                 </ul>
-                                                        </li>         
+                                                        </li>
                                                         <li class="dropdown">
                                                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Contact <b class="caret"></b></a>
                                                                 <ul class="dropdown-menu">
@@ -141,7 +141,7 @@
                                                                         <li class="divider"></li>
                                                                         <li><a href="404-rtl.html">404 RTL</a></li>
                                                                 </ul>
-                                                        </li>                           
+                                                        </li>
                                                         <li class="dropdown">
                                                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sign Up <b class="caret"></b></a>
                                                                 <ul class="dropdown-menu">
@@ -149,7 +149,7 @@
                                                                         <li class="divider"></li>
                                                                         <li><a href="sign-up-rtl.html">Sign Up RTL</a></li>
                                                                 </ul>
-                                                        </li>    
+                                                        </li>
                                                         <li class="dropdown">
                                                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sign In <b class="caret"></b></a>
                                                                 <ul class="dropdown-menu">
@@ -157,7 +157,7 @@
                                                                         <li class="divider"></li>
                                                                         <li><a href="sign-in-rtl.html">Sign In RTL</a></li>
                                                                 </ul>
-                                                        </li>    
+                                                        </li>
                                                         <li class="dropdown">
                                                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Elements <b class="caret"></b></a>
                                                                 <ul class="dropdown-menu">
@@ -165,12 +165,12 @@
                                                                         <li class="divider"></li>
                                                                         <li><a href="elements-rtl.html">Elements RTL</a></li>
                                                                 </ul>
-                                                        </li>                
+                                                        </li>
                                                 </ul>
                                         </div>
                                 </div>
-                        </nav><!--#main-nav-->  
-                        
+                        </nav><!--#main-nav-->
+
                         <!-- FULL WIDTH CAROUSEL -->
                         <div id="carousel-large" class="carousel slide carousel-large no-mobile" data-ride="carousel">
                                 <!-- Indicators-->
@@ -196,10 +196,10 @@
                                                                         volutpat mi eu, porta nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada
                                                                         Pellentesque lacinia varius quam. Nullam aliquet ante ac nibh convallis. Sed pulvinar eu eros ut blandit..
                                                                 </p>
-                                                        </div>                             
+                                                        </div>
                                                 </div>
                                         </div>
-                                        
+
                                         <div class="item">
                                                 <div data-alt   ="Fames ac turpis egestas Curabitur scelerisque sagittis lectus ac consequat Mauris dictum sollicitudin tincidunt"
                                                      data-small ="http://placehold.it/768x384"
@@ -214,10 +214,10 @@
                                                                         Nulla gravida erat nisi, sed laoreet nulla fringilla id. Maecenas tristique erat ullamcorper purus consequat egestas,
                                                                         eu feugiat ipsum eleifend vel eget auctor risus facilisis. Suspendisse ornare feugiat leo...
                                                             </p>
-                                                        </div>                            
+                                                        </div>
                                                 </div>
                                         </div>
-                                        
+
                                         <div class="item">
                                                 <div data-alt   ="Vivamus libero purus placerat id varius ut facilisis ut odio consequat Mauris dictum sollicitudin tincidunt"
                                                      data-small ="http://placehold.it/768x384"
@@ -228,11 +228,11 @@
                                                         <div><a href="#"><h1>Vivamus libero purus placerat id varius ut facilisis ut odio dictum sollicitudin tincidunt</h1></a></div>
                                                         <div class="hidden-xs hidden-sm">
                                                                 <p>
-                                                                        Vestibulum id urna at magna dignissim sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere 
+                                                                        Vestibulum id urna at magna dignissim sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
                                                                         Nam sagittis neque et lectus varius imperdiet. Phasellus blandit viverra diam, et tincidunt arcu tempus ut.
                                                                         Nulla gravida erat nisi, sed laoreet nulla fringilla id. Maecenas tristique erat ullamcorper purus consequat egestas...
                                                                 </p>
-                                                        </div>                            
+                                                        </div>
                                                 </div>
                                         </div>
                                 </div>
@@ -245,4 +245,4 @@
                                 <a class="left carousel-control" href="#carousel-large" data-slide="prev"><span class="fa fa-chevron-left"></span></a>
                                 <a class="right carousel-control" href="#carousel-large" data-slide="next"><span class="fa fa-chevron-right"></span></a>
                         </div>
-                </header>  
+                </header>
